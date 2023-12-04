6044e35ce452e04f01a260cbea191f982a3e4efc
robostac <res@robostac.co.uk>
2023-12-04 11:09:05 +0000

    Day 3+4

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -4,4 +4,69 @@ use std::io;
 fn main() {
     let stdin = io::stdin();
     let mut scan = Scanner::new(stdin.lock());
+    let mut grid = HashMap::new();
+    let mut symbols = HashMap::new();
+    let mut y = 1i64;
+    let mut mx = 0;
+    let mut my = 0;
+    while let Some(s) = scan.next::<String>() {
+        for (x, v) in s.chars().enumerate() {
+            let x = x as i64 + 1;
+            if v == '.' {
+                continue;
+            }
+            if let Some(c) = v.to_digit(10) {
+                grid.insert((x, y), c);
+            } else {
+                symbols.insert((x, y), v);
+            }
+            mx = mx.max(x);
+            my = my.max(y);
+        }
+        y += 1;
+    }
+
+    let mut ans = 0;
+    let mut gears = HashMap::new();
+    for y in 1..=my {
+        let mut value = 0;
+        let mut symbol = false;
+        let mut syms = HashSet::new();
+        for x in 1..=(mx + 1) {
+            if let Some(p) = grid.get(&(x, y)) {
+                value *= 10;
+                value += p;
+                for dx in -1..=1 {
+                    for dy in -1..=1 {
+                        symbol |= symbols.contains_key(&(x + dx, y + dy));
+                        syms.insert((x + dx, y + dy));
+                    }
+                }
+            } else {
+                if value > 0 {
+                    if symbol {
+                        ans += value;
+                    }
+                    for x in syms {
+                        gears.entry(x).or_insert(Vec::new()).push(value);
+                    }
+                    syms = HashSet::new();
+                    value = 0;
+                    symbol = false;
+                }
+            }
+        }
+    }
+    let mut p2 = 0;
+    for (x, s) in symbols {
+        if s == '*' {
+            if let Some(v) = gears.get(&x) {
+                if v.len() == 2 {
+                    p2 += v[0] * v[1];
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+    println!("{:?}", p2);
 }
--- 4/src/main.rs
+++ 4/src/main.rs
@@ -1,7 +1,42 @@
-use scanner::Scanner;
 use std::collections::*;
 use std::io;
+use std::io::BufRead;
+
 fn main() {
     let stdin = io::stdin();
-    let mut scan = Scanner::new(stdin.lock());
+    let mut cards = Vec::new();
+
+    let make_set = |x: &str| {
+        x.split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<HashSet<_>>()
+    };
+
+    for l in stdin.lock().lines() {
+        let s = l.unwrap();
+        if let Some((_, v)) = s.split_once(':') {
+            let (win, have) = v.split_once('|').unwrap();
+
+            let h = make_set(have);
+            let w = make_set(win);
+
+            let count = w.intersection(&h).count();
+
+            cards.push(count);
+        }
+    }
+    let mut counts = vec![1; cards.len()];
+
+    let mut p1 = 0;
+    for (i, x) in cards.iter().enumerate() {
+        if *x > 0 {
+            p1 += 1 << (x - 1);
+            for j in (i + 1)..(cards.len().min(i + *x + 1)) {
+                counts[j] += counts[i]
+            }
+        }
+    }
+
+    println!("{}", p1);
+    println!("{:?}", counts.iter().sum::<usize>());
 }
