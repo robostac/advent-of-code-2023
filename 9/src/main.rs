29a67ccec7bbfd41f138a3c7d5a28ab993c8778f
robostac <res@robostac.co.uk>
2023-12-09 12:02:28 +0000

    Day9

--- 9/src/main.rs
+++ 9/src/main.rs
@@ -1,7 +1,43 @@
 use scanner::Scanner;
 use std::collections::*;
 use std::io;
+use std::io::BufRead;
+
+fn solve_val(x: &Vec<i64>) -> i64 {
+    let mut test = vec![x.clone()];
+    while test.last().unwrap().len() != 1 {
+        let z = test
+            .last()
+            .unwrap()
+            .iter()
+            .enumerate()
+            .skip(1)
+            .map(|(i, x)| x - test.last().unwrap()[i - 1])
+            .collect();
+        test.push(z)
+    }
+
+    for i in (0..test.len()).rev().skip(1) {
+        let v = test[i].last().unwrap() + test[i + 1].last().unwrap();
+        test[i].push(v);
+    }
+    *test[0].last().unwrap()
+}
+
 fn main() {
     let stdin = io::stdin();
-    let mut scan = Scanner::new(stdin.lock());
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for l in stdin.lock().lines().map(|x| x.unwrap()) {
+        let mut p = l
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+
+        p1 += solve_val(&p);
+        p.reverse();
+        p2 += solve_val(&p);
+    }
+    println!("{}", p1);
+    println!("{}", p2);
 }
